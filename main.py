08cffadd5b0082487bb9153388b333b3f8065ebe
Spencer Peters <spencer@goebelpeters.com>
2020-04-22 11:46:06 -0400

    Refactored simulation and added visualization for pos/not pos; precedent/not prec

--- main.py
+++ main.py
@@ -3,22 +3,6 @@ from matplotlib import pyplot as plt
 from sklearn.neighbors import NearestNeighbors
 import pdb
 
-# set the domain
-
-
-def run(decision_rule, judge_distribution,case_sampling_func, precedent_cases, precedent_outcomes, N):
-	# run the specified decition process for N
-	history = []
-	for _ in range(N):
-		# sample a case 
-		x = case_sampling_func()
-		decision, set_precedent = decision_rule(x, judge_distribution, precedent_cases, precedent_outcomes)
-		history.append((x,decision, set_precedent))
-	return(precedent_cases, precedent_outcomes, history)
-
-
-
-
 def find_knn(all_cases, target_case, k):
 	# returns nearest neighbors and distances
 	nbrs = NearestNeighbors(n_neighbors=k, algorithm='ball_tree').fit(all_cases)
@@ -71,11 +55,26 @@ def scatter_cases(history):
 	plt.scatter([e[0] for e in cases],[e[1] for e in cases])
 	plt.show()
 
+def is_positive(entry):
+	return entry[1]
+
+def is_precedent(entry):
+	return entry[2]
+
+def case(entry):
+	return entry[0]
+
 def scatter_cases_with_outcomes(history):
-	positive_cases = [e[0] for e in history if e[2]]
-	negative_cases = [e[0] for e in history if not e[2]]
-	plt.scatter([e[0] for e in positive_cases],[e[1] for e in positive_cases])
-	plt.scatter([e[0] for e in negative_cases],[e[1] for e in negative_cases])
+	# positive_cases = [e[0] for e in history if e[1]]
+	# negative_cases = [e[0] for e in history if not e[1]]
+	colors = ["green" if is_positive(e) else "red" for e in history]
+	edgecolors = ["black" if is_precedent(e) else "none" for e in history]
+	plt.scatter([case(e)[0] for e in history], [case(e)[1] for e in history], 
+		c = colors, edgecolors=edgecolors)
+	# plt.scatter([c[0] for c in positive_cases],[c[1] for c in positive_cases],
+	# 	c = "green")
+	# plt.scatter([c[0] for c in negative_cases],[c[1] for c in negative_cases],
+	# 	c = "red")
 	plt.show()
 
 def loss(history, judge_distribution):
@@ -92,33 +91,58 @@ def loss(history, judge_distribution):
 	return(average_loss)
 
 
-# set domain
+def run(decision_rule, judge_distribution, case_sampling_func, N):
+	"""
+	Run a simulated legal system.
+
+	At each time step, a case c in D is randomly drawn from a fixed distribution Pr_D(.). It is judged (assigned a boolean label) according to a decision rule R. R either sets precedent (outputs true with probability Pr_J(true|c) for some fixed distribution Pr_J(., .))
+	or does not (labels c somehow based on previous decisions). 
+	Maintains history (list) consisting of tuples (c, decision, set_precedent),
+		where c is a case, decision is its label, and set_precedent is True iff the decision rule set a precedent when judging c.
+
+	Returns: history after judging N cases. 
+
+	Parameters:
+	N (int): Number of cases to run.
+	judge_distribution (Map D -> [0, 1]): judge_distribution(c) = Pr_J(1|c)
+	case_sampling_func (Map () -> D): case_sampling_func() is a sample from Pr_D.
+	decision_rule (Map D -> Pr_J(1|.) -> precedents -> outcomes -> {0, 1} * bool):
+		Represents the decision rule R.
+	"""
+	# initialize precedence data
+  # case -> outcome dictinary
+	precedent_cases = []
+	precedent_outcomes = {}
+	# run the specified decition process for N
+	history = []
+	for _ in range(N):
+		# sample a case 
+		x = case_sampling_func()
+		decision, set_precedent = decision_rule(x, judge_distribution, precedent_cases, precedent_outcomes)
+		history.append((x,decision, set_precedent))
+	return history
+
+
 d = 2 # dimension
 l = 0 # left border
 r = 1 # right border
-# set the judge opinion distribution
-judge_distribution = lambda x: constant_func(x,0.7)
-# how to sample cases from domain
-case_sampling_func = lambda: uniform_sample(d,l,r)
-# set decision rule
-k=7 # make it odd to avoid draws
+k = 7 # make it odd to avoid draws
 max_distance = 0.05 # distance within which to look for precedents
-decision_rule = lambda x, judge_distribution, precedents, outcomes: distance_limited_precedence(x, judge_distribution, precedents, outcomes, k, max_distance)
-
-# initialize precedence data
-precedent_cases = []
-precedent_outcomes = {} # case -> outcome dictinary
-
-# number of cases to run
+judge_distribution = lambda x: constant_func(x,0.7)
 N = 10000
 
-precedent_cases, precedent_outcomes, history = run(decision_rule, judge_distribution,case_sampling_func, precedent_cases, precedent_outcomes, N)
-h_outcomes = [e[1] for e in history] 
-h_set_precedent = [e[2] for e in history]
+history = run(N=N,
+							judge_distribution=judge_distribution,
+							case_sampling_func = lambda: uniform_sample(d, l, r),
+							decision_rule = lambda x, judge_distribution, precedents, 						outcomes: distance_limited_precedence(x, judge_distribution, 			precedents, outcomes, k, max_distance)
+)
 
 # scatter_cases(history)
 scatter_cases_with_outcomes(history)
 loss = loss(history, judge_distribution)
+
+h_outcomes = [e[1] for e in history] 
+h_set_precedent = [e[2] for e in history]
 
 print("Average loss per case:")
 print(loss)

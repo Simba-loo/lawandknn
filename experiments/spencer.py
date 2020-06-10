3eed66076e2dbd4093ef11debaa94f903a4c077e
Spencer Peters <spencer@goebelpeters.com>
2020-06-10 11:36:48 -0700

    Modified my experiment

--- experiments/spencer.py
+++ experiments/spencer.py
@@ -7,16 +7,16 @@ from simulation import run, constant_func, scatter_cases_with_outcomes
 from simulation import uniform_sample, loss
 from decider import DistanceLimitedDecider, DistanceLimitedForgetfulDecider
 from decider import DistanceLimitedDropoutDecider, CaseByCaseDecider
-from decider import SuperPrecedentsDecider
+from decider import SuperPrecedentsDecider, DistanceLimitedHarmonicOverrulingDecider, DistanceLimitedConstantOverrulingDecider, DistanceLimitedTimedDecider
 
 def main():
   d = 2 # dimension
   l = 0 # left border
   r = 1 # right border
-  k = 7 # make it odd to avoid draws
+  k = 51 # make it odd to avoid draws
   # distance within which to look for precedents
   # max_distance = 0.05 
-  max_distance = 0.05
+  max_distance = 0.1
 
   judge_distribution = lambda x: constant_func(x,0.7)
   # judge_distribution = lambda x: 0.5 + 0.5 * math.sin(2 * math.pi * x[0])
@@ -45,12 +45,17 @@ def main():
                 judge_distribution=judge_distribution,
                 case_sampling_func = lambda: uniform_sample(d, l, r),
                 # decider = CaseByCaseDecider()
-                # decider = DistanceLimitedDecider(k, max_distance),
-                # decider = DistanceLimitedDecider(k, max_distance, 1000),
+                # decider = DistanceLimitedDecider(k=k, max_distance=max_distance),
+                # decider = DistanceLimitedConstantOverrulingDecider(k=k, max_distance=max_distance,
+                # overruling_probability=0.1),  
                 # decider = DistanceLimitedDropoutDecider(k, max_distance, 
                   # half_life = 10000000,
                   # dropout_interval = 100)
-                decider = SuperPrecedentsDecider([k,k], [max_distance,    3*max_distance],)
+                # decider = SuperPrecedentsDecider([k,k], [max_distance,    3*max_distance],)
+                decider = DistanceLimitedTimedDecider(
+                    k_of_self = lambda self: math.floor(math.log(max(self.current_time, math.exp(7)))),
+                    max_distance_of_self = max_distance
+                )
 )
 
   experiment_loss = loss(history, judge_distribution)
@@ -67,6 +72,7 @@ def main():
 
   # scatter_cases(history)
   scatter_cases_with_outcomes(history)
+  # plt.savefig("cases_with_outcomes.fig")
 
   # when are precedents established? My guess is system exhibits phase transition
   # Not as sharp as I expected for small N, but there is a phase transition
@@ -75,7 +81,19 @@ def main():
   # Is this problem alleviated by decaying precedents?
   precedent_indicators = [1 if e.set_precedent else 0 for e in history]
   precedents_set_up_to_t = np.cumsum(np.asarray(precedent_indicators))
+  nonprecedents_set_up_to_t = np.cumsum(np.logical_not(np.asarray(precedent_indicators)))
   plt.plot(precedents_set_up_to_t)
+  plt.xlabel("t")
+  plt.ylabel("precedents set up to t")
+  # plt.savefig("precedents_up_to_t.fig")
+  plt.show()
+  nonprec_loss_over_time = [e.nonprecedent_loss for e in history]
+  fractional_nonprec_loss_over_time = \
+      np.asarray(nonprec_loss_over_time) / np.asarray(nonprecedents_set_up_to_t)
+  plt.plot(fractional_nonprec_loss_over_time)
+  plt.xlabel("t")
+  plt.ylabel("fractional nonprecedent loss up to t")
+  # plt.savefig("loss_up_to_t.fig")
   plt.show()
 
 if __name__ == "__main__":
